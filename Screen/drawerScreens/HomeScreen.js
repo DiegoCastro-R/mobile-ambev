1b241748e53ebddc969252ccbd44a0724fb37c4c
Diego Rodrigues <diegocastro_r@yahoo.com>
2020-07-05 20:03:03 -0300

    Adding,Google Maps

--- Screen/drawerScreens/HomeScreen.js
+++ Screen/drawerScreens/HomeScreen.js
@@ -1,15 +1,36 @@
 //Import React
 import React from 'react';
-
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 //Import all required component
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 const HomeScreen = () => {
   global.currentScreenIndex = 'HomeScreen';
   return (
-    <View style={{flex: 1, alignItems: 'center', marginTop: 100}}>
-      <Image source={require('../../Image/maps.png')} />
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      />
     </View>
   );
 };
 export default HomeScreen;
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
